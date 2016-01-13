88496e2d7fc35d1aa058184301ebf76b1009f4b0
Tinus <jacksontinus96@gmail.com>
2016-01-13 17:18:57 +0200

    Structure & Routes

---
+++ socialcircle/platforms/android/assets/www/js/socialcircle.js
@@ -0,0 +1,37 @@
+ var socialcircle = angular.module("socialcircle", ['ngRoute','ngCordova']);
+ 
+
+socialcircle.config(function($routeProvider,$locationProvider) {
+        $routeProvider 
+           
+           .when('/main', {
+			   templateUrl: 'templates/main.html'
+			})
+			
+			    .when('/capture', {
+			   templateUrl: 'templates/captureImage.html'
+			})
+			
+			
+			.otherwise({
+			redirectTo: '/main'
+		});
+	
+	   
+    });
+
+
+
+
+
+
+// Add a valid option for exit 
+document.addEventListener("deviceready", function () {
+       
+});
+
+		
+ function menuKeyDown() {
+	//	alert('Menu button pressed.');
+	}
+	
